5ce6d09d9dbf507fd0017fba772ff2589bb03c0b
pradeep <pradeeppalara@gmail.com>
2023-06-12 23:11:47 +0530

    DONE

--- models/url.model.js
+++ models/url.model.js
@@ -1,15 +1,16 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
+
 const ShortUrlSchema = new Schema({
-  url: {
-    type: String,
-    required: true,
-  },
-  shortId: {
-    type: String,
-    required: true,
-  },
+    url: {
+        type: String,
+        required: true,
+    },
+    shortId: {
+        type: String,
+        required: true,
+    },
 })
 
 const ShortUrl = mongoose.model('shortUrl', ShortUrlSchema)
